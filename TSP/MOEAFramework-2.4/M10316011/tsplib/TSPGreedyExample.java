767c1cbd5e854ca91cd2a08371e4705606123a65
chuanrou <chuanrou@hotmail.com>
2015-05-05 13:32:57 +0800

    Remove unuse lib

--- TSP/MOEAFramework-2.4/M10316011/tsplib/TSPGreedyExample.java
+++ TSP/MOEAFramework-2.4/M10316011/tsplib/TSPGreedyExample.java
@@ -26,9 +26,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Properties;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
-import java.util.logging.SimpleFormatter;
 import java.util.logging.Logger;
 
 import javax.swing.JFrame;
